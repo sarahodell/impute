8c4b6bb1594b7c00cc1af81ff7e58d1b051abe98
Sarah Odell <sgo7@cornell.edu>
2018-07-10 12:26:31 -0700

    qtl2 files

--- qtl2/foundergeno.py
+++ qtl2/foundergeno.py
@@ -12,7 +12,8 @@ import argparse
 def parse_args():
     """ -h for info on arguments
     """
-    parser = argparse.ArgumentParser(description="""Program description""")
+    parser = argparse.ArgumentParser(description="""Program description: Takes a vcf file and converts it to a csv format with founder rows and marker genotypes as columns. Genotypes are assumed homozygous, with reference allele as A and alternate alleles as B.                                                             
+This csv file is formatted for use with R/qtl2""")
     parser.add_argument("infile",type=str,help="""The input vcf file""")
     parser.add_argument("outfile",type=str,help="""The output csv filename""")
     args=parser.parse_args()
