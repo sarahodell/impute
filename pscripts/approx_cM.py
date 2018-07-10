ef89eddcd048f8f9bc24987bdb315acb039ede76
Sarah G Odell <sodell@bigmem2.farm.cse.ucdavis.edu>
2018-07-10 15:02:26 -0700

    approx cM

--- pscripts/approx_cM.py
+++ pscripts/approx_cM.py
@@ -8,7 +8,7 @@ from scipy import stats
 
 def arg_parse():
     parser=argparse.ArgumentParser(description="""Program description: Uses the Ogut 2015 maize genetic map to approximate the genetic distance between two physical positions""")
-    parser.add_argument("chr",type=int,help="""Chromosome number""")
+    parser.add_argument("chrom",type=int,help="""Chromosome number""")
     parser.add_argument("start",type=float,help="""Start position (bp)""")
     parser.add_argument("end",type=float,help="""End position (bp)""")
     parser.add_argument("--ref",type=str,default="v4",help="""Reference version of physical positions (either v2, v3, or v4) """)
@@ -16,7 +16,7 @@ def arg_parse():
     return args
 
 
-def approx_cM(chr,start,end,ref='v4'):
+def approx_cM(chrom,start,end,ref='v4'):
     """Arguments:
     chr: (int) chromosome number 1..10
     start: (int) physical bp position of start
@@ -26,16 +26,18 @@ def approx_cM(chr,start,end,ref='v4'):
     Returns:
     The approximate genetic distance between the start and end positions based on the Ogut 2015 genetic map (int)
     """
-    ogutpath=format('/group/jrigrp/Share/annotations/genetic_map/ogut2015/ogut_fifthcM_map_agp{0}.txt',ref)
-    ogutmap=pd.read_table(ogutpath,sep='\t',header=None,
+    ogutpath='/group/jrigrp/Share/annotations/genetic_map/ogut2015/ogut_fifthcM_map_agp{0}.txt'.format(ref)
+    ogutmap=pd.read_table(ogutpath,sep='\s+',header=None,
                           names=['SNP_ID','SNP_newID','chr','pos','cM'])
     ogutmap.dropna(axis=0,inplace=True)
     ogutmap['chr']=pd.to_numeric(ogutmap['chr'])
+    ogutmap['pos']=pd.to_numeric(ogutmap['pos'])
     #Grab the chromosome
-    cmap=ogutmap[ogutmap['chr']==chr]
+    cmap=ogutmap[ogutmap['chr']==chrom]
+    cmap.reset_index(inplace=True)
     #Closest ogut map markers above and below the start and end postions
-    below=cmap.iloc[cmap[cmap['pos']<=start].idxmax(),]
-    above=cmap.iloc[cmap[cmap['pos']>=end].idxmin(),]
+    below=cmap.iloc[cmap[cmap['pos']<=start]['pos'].idxmax(),]
+    above=cmap.iloc[cmap[cmap['pos']>=end]['pos'].idxmin(),]
     xpoints=[below['pos'],above['pos']]
     ypoints=[below['cM'],above['cM']]
     #Linear regression of genetic position on physical position using two closest points
@@ -49,5 +51,5 @@ def approx_cM(chr,start,end,ref='v4'):
 
 if __name__ == "__main__":
     args=arg_parse()
-    distance=approx_cM(args.chr,args.start,args.end,args.ref)
-    print(format("The approximate genetic distance between {0} and {1} is {2} cM ({3} cM/bp)",args.start,args.end,distance,distance/(args.end-args.start)))
+    distance=approx_cM(args.chrom,args.start,args.end,ref=args.ref)
+    print("The approximate genetic distance between {0} and {1} is {2} cM ({3} cM/bp)".format(int(args.start),int(args.end),round(distance,3),round(distance/(args.end-args.start),6)))
