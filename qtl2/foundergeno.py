2b46d13ab74f273b5914e834616f01f66b504222
Sarah G Odell <sodell@farm.cse.ucdavis.edu>
2018-07-11 15:42:26 -0700

    generalized qtl2 scripts

--- qtl2/foundergeno.py
+++ qtl2/foundergeno.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+from subprocess import Popen,PIPE
 
 def parse_args():
     """ -h for info on arguments
@@ -9,7 +10,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="""Program description:
                                      Takes a vcf file and converts it to a csv format with founder as rows and marker genotypes as columns
                                      with nucleotide information encoded as A for reference allele and B for alternate alleles.
-                                     This csv file is formatted for use with R/qtl2
+                                     This csv file is formatted for use with R/qtl2. It requires that bcftools be installed.
                                      """)
     parser.add_argument("infile",type=str,help="""The input vcf file""")
     parser.add_argument("outfile",type=str,help="""The output csv filename""")
@@ -17,7 +18,13 @@ def parse_args():
     args=parser.parse_args()
     return args
 
+def get_tmp(vcf):
+    process=Popen(["bcftools","query","-f","'%ID,%CHROM,%POS,[,%GT]\n'",vcf],stdout=PIPE,stderr=PIPE)
+    stdout,stderr=process.communicate()
+    print(stderr)
+    return stdout
 
+    
 def get_foundergenofile():
     args=parse_args()
     txt='ind'
@@ -27,21 +34,22 @@ def get_foundergenofile():
         for line in ffile:
             samples[key]=line[:-1]
             key+=1
-    with open(args.infile,'r') as infile:
-        for line in infile:
-            info = line.split(',')
-            marker = info[0]
-            txt+=',{0}'.format(marker)
-            count=0
-            for i in info[3:]:
-                if './.' in i:
-                    n='NA'
-                elif '0' in i:
-                    n='A'
-                else:
-                    n='B'
-                samples[count]+=',{0}'.format(n)    
-                count+=1
+    tmp=get_tmp(args.infile)
+    markers=tmp.split('\n')[:-1]
+    for line in markers:
+        info = line.split(',')
+        marker = info[0]
+        txt+=',{0}'.format(marker)
+        count=0
+        for i in info[3:-1]:
+            if './.' in i:
+                n='NA'
+            elif '0' in i:
+                n='A'
+            else:
+                n='B'
+            samples[count]+=',{0}'.format(n)    
+            count+=1
     for k in range(key):
         txt+='\n'+samples[k]
     print "Writing out to {0}".format(args.outfile)
