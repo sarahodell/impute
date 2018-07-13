2f56136f8cb9e57c6cb73f3cfbbcfc5852925521
Sarah G Odell <sodell@farm.cse.ucdavis.edu>
2018-07-12 17:55:08 -0700

    made foundergeno faster

--- qtl2/foundergeno.py
+++ qtl2/foundergeno.py
@@ -3,58 +3,74 @@
 import sys
 import argparse
 from subprocess import Popen,PIPE
+import string
 
 def parse_args():
     """ -h for info on arguments
     """
     parser = argparse.ArgumentParser(description="""Program description:
                                      Takes a vcf file and converts it to a csv format with founder as rows and marker genotypes as columns
-                                     with nucleotide information encoded as A for reference allele and B for alternate alleles.
+                                     with nucleotide information encoded as A for reference allele and B for alternate alleles. Founder lines will be given letter codes based on the order that they are listed in the vcf file ("A" for the first sample and so on).
                                      This csv file is formatted for use with R/qtl2. It requires that bcftools be installed.
                                      """)
     parser.add_argument("infile",type=str,help="""The input vcf file""")
     parser.add_argument("outfile",type=str,help="""The output csv filename""")
-    parser.add_argument("founders",type=str,help="""File with a list of founders in the order they are listed in the vcf file""")
     args=parser.parse_args()
     return args
 
-def get_tmp(vcf):
-    process=Popen(["bcftools","query","-f","'%ID,%CHROM,%POS,[,%GT]\n'",vcf],stdout=PIPE,stderr=PIPE)
+def get_founders(vcf):
+    """Gets a list of the samples in the vcf file"""
+    process=Popen(["bcftools", "query","-l",vcf],stdout=PIPE,stderr=PIPE)
     stdout,stderr=process.communicate()
     print(stderr)
-    return stdout
+    founders=stdout.split('\n')[:-1]    
+    return founders
+
+def get_tmp(vcf):
+    process=Popen(['bcftools','query','-f','%ID[\tGT=%GT]\n',vcf],stdout=PIPE,stderr=PIPE)
+    stdout,stderr=process.communicate()
+    info = stdout.split('\n')[:-1]
+    print(stderr)
+    return info
 
     
 def get_foundergenofile():
     args=parse_args()
     txt='ind'
     samples={}
-    key=0
-    with open(args.founders,'r') as ffile:
-        for line in ffile:
-            samples[key]=line[:-1]
-            key+=1
-    tmp=get_tmp(args.infile)
-    markers=tmp.split('\n')[:-1]
-    for line in markers:
-        info = line.split(',')
-        marker = info[0]
-        txt+=',{0}'.format(marker)
-        count=0
-        for i in info[3:-1]:
-            if './.' in i:
-                n='NA'
-            elif '0' in i:
-                n='A'
-            else:
-                n='B'
-            samples[count]+=',{0}'.format(n)    
-            count+=1
-    for k in range(key):
-        txt+='\n'+samples[k]
+    founders=get_founders(args.infile)
+    numf=len(founders)
+    print(founders)
+    f=''
+    lcode=string.ascii_uppercase
+    for l in range(1,numf+1):
+        samples[l]=[lcode[l-1]]
+        f+='{0},{1}\n'.format(founders[l-1],lcode[l-1])
+    print "Writing founder codes to FounderCodes.csv"
+    with open('FounderCodes.csv','w') as ffile:
+        ffile.write(f)
+    info=get_tmp(args.infile)
+    for s in range(1,numf+1):
+        for i in info:
+            split=i.split('\t')
+            marker = split[0]
+            txt+=','+marker
+            count=1
+            for n in split[1:]:
+                if './.' in n:
+                    n='NA'
+                elif '0' in n:
+                    n='A'
+                else:
+                    n='B'
+                samples[count].append(n)
+                count+=1
+    for j in samples.keys():
+        txt+='\n'+ ','.join(samples[j])
     print "Writing out to {0}".format(args.outfile)
     with open(args.outfile,'w') as outfile:
         outfile.write(txt)
+
 
 
 
